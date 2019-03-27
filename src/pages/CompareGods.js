4a6abf3f49d0a02ada37db661c0921525ddbc02c
alloca <franz.diebold@tum.de>
2019-03-27 22:05:44 +0100

    remove CompareGodsList component

--- src/pages/CompareGods.js
+++ src/pages/CompareGods.js
@@ -4,11 +4,16 @@ import PropTypes from 'prop-types';
 
 import { Section, Hero, HeroBody, Container, Columns, Column, Title } from 'bloomer';
 
-import CompareGodsList from '../components/CompareGodsList/CompareGodsList';
+import GodPair from '../components/GodPair/GodPair';
+import LoadingIndicator from '../components/LoadingIndicator/LoadingIndicator';
+import { godsCounterpartsSortedFilter } from '../utils/godsSortedFilter';
 import GodPropType from '../utils/GodPropType';
 
 function Compare(props) {
-    const { godCounterparts, isLoading, godsMap, filterText } = props;
+    const { godCounterparts, isLoading, filterText, godsMap } = props;
+
+    const compareGods = godsCounterpartsSortedFilter(godCounterparts, filterText)
+        .map(godPair => <GodPair key={`${godPair.greek.id}-${godPair.roman.id}`} godPair={godPair} godsMap={godsMap} />);
 
     return (
         <div>
@@ -25,12 +30,11 @@ function Compare(props) {
                                 <Column isSize={{mobile: 12, tablet: 12, desktop: 10}}>
                                     <Title>Compare Greek and Roman Gods and Goddesses</Title>
 
-                                    <CompareGodsList
-                                        godCounterparts={godCounterparts}
-                                        isLoading={isLoading}
-                                        filterText={filterText}
-                                        godsMap={godsMap}
-                                    />
+                                    {
+                                        isLoading ?
+                                            <LoadingIndicator /> :
+                                            compareGods
+                                    }
                                 </Column>
                             </Columns>
                         </Container>
