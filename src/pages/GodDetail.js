cf16175ac07a4fb0ad10d2f8a4eb1f408db0b303
alloca <franz.diebold@tum.de>
2019-03-31 18:25:15 +0200

    redesign god detail

--- src/pages/GodDetail.js
+++ src/pages/GodDetail.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import { Section, Container } from 'bloomer';
+import {
+    Section,
+    Breadcrumb, BreadcrumbItem,
+    Columns, Column,
+} from 'bloomer';
 
 import God from '../components/God/God';
 import LoadingIndicator from '../components/LoadingIndicator/LoadingIndicator';
+import { capitalizeFirstCharacter } from '../utils/capitalizeFirstCharacter';
 import NotFound from './NotFound';
 import GodPropType from '../utils/GodPropType';
 
@@ -19,17 +25,31 @@ function GodDetail(props) {
 
     return (
         <Section className="has-heaven-bg">
-            <Container>
-                {
-                    isLoading ?
-                        <LoadingIndicator /> :
-                        <God
-                            god={god}
-                            showDetailed={true}
-                            godsMap={godsMap}
-                        />
-                }
-            </Container>
+            <Columns isCentered>
+                <Column isSize={{mobile: 12, tablet: 10, desktop: 8}}>
+                    {
+                        isLoading ?
+                            <LoadingIndicator /> :
+                            <div>
+                                <Breadcrumb>
+                                    <ul>
+                                        <BreadcrumbItem>
+                                            <Link to={`/${god.type}`}>{capitalizeFirstCharacter(god.type)}</Link>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbItem isActive>
+                                            <a href>{god.name}</a>
+                                        </BreadcrumbItem>
+                                    </ul>
+                                </Breadcrumb>
+                                <God
+                                    god={god}
+                                    showDetailed={true}
+                                    godsMap={godsMap}
+                                />
+                            </div>
+                    }
+                </Column>
+            </Columns>
         </Section>
     );
 }
