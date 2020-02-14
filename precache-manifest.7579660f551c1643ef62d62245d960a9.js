6d624811402691c47ae195ed78f1e511173f8778
Franz Diebold (via Travis CI) <franz.diebold@tum.de>
2020-02-14 07:41:52 +0000

    Deploy FranzDiebold/lyktos to github.com/FranzDiebold/lyktos.git:gh-pages

---
+++ precache-manifest.7579660f551c1643ef62d62245d960a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf82350f9f2ac00c2ef395e20508dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88a26ef30d54e048b492",
+    "url": "/static/css/main.2ee61cba.chunk.css"
+  },
+  {
+    "revision": "5a70d269bb2974b0898d",
+    "url": "/static/js/2.b455de0a.chunk.js"
+  },
+  {
+    "revision": "2d290c4b61d5008117f7b1cd947f0c78",
+    "url": "/static/js/2.b455de0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a26ef30d54e048b492",
+    "url": "/static/js/main.2c8b4569.chunk.js"
+  },
+  {
+    "revision": "211d372c7bfc3c55ec5f",
+    "url": "/static/js/runtime-main.bc90c374.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd79ee5d5684b5d9d9cdd4915b5432ff.js"
+  "/precache-manifest.7579660f551c1643ef62d62245d960a9.js"
 );
 
 self.addEventListener('message', (event) => {
