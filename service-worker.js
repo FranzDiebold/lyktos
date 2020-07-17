bd7000e25cabb7c5f84dd3b7e7899aab8604c56b
Franz Diebold (via Travis CI) <35296779+FranzDiebold@users.noreply.github.com>
2020-07-17 14:13:38 +0000

    Deploy FranzDiebold/lyktos to github.com/FranzDiebold/lyktos.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f53c4c5267a04338ea142b5407468fa.js"
+  "/precache-manifest.d493f42ebc83559d06114d2d37f7495f.js"
 );
 
 self.addEventListener('message', (event) => {
